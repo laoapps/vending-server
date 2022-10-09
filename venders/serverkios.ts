0ce3915fe011332b95e548136a53e9cc1eda387e
touy <touya.ra@gmail.com>
2022-10-09 22:39:25 +0700

    update

--- venders/serverkios.ts
+++ venders/serverkios.ts
@@ -1,12 +1,8 @@
 
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import helmet from 'helmet';
 
-import * as WebSocket from 'ws';
 import { KiosServer } from './api/kios';
 import { SocketKiosClient } from './api/socketClient.kios';
-import { PrintSucceeded } from './services/service';
+
 
 
 new KiosServer(new SocketKiosClient())
