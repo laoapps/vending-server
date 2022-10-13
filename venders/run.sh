05db038e46fb9b2afb7508428029b17e8d32ae0c
touy <touya.ra@gmail.com>
2022-10-14 05:48:37 +0700

    udpate run.sh

--- venders/run.sh
+++ venders/run.sh
@@ -1,2 +1,2 @@
 #!/data/data/com.termux/files/usr/bin/sh
-pm2 start /data/data/com.termux/files/home/projects/vending-server/venders/dist/serverVenderZDM8.js
+node /data/data/com.termux/files/usr/bin/pm2 resurrect
