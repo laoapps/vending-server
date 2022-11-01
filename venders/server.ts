8aca9f373635775a8f86f4a4cf2bab531aa02b13
touy <touya.ra@gmail.com>
2022-11-01 21:08:30 +0700

    update

---
+++ venders/server.ts
@@ -0,0 +1,59 @@
+
+import { SocketKiosClient } from './api/socketClient.kios';
+import { SocketClientM102 } from './api/socketClient.m102';
+import { SocketClientVMC } from './api/socketClient.vmc';
+import { SocketClientZDM8 } from './api/socketClient.zdm8';
+
+const env = process.env.pname;
+if(env=='nv9'){
+const x =new SocketKiosClient();
+process.on('exit', (code: number) => {
+    // port.close();
+});
+}else if(env =='zdm8'){
+    const clients =[
+
+        new SocketClientZDM8()];
+      
+        process.on('exit', (code:number)=>{
+          console.log('exit code',code);
+          
+          clients.forEach(v=>{
+            v.close();
+          })
+        });
+      
+}else if(env=='m102'){
+    const clients =[
+
+        new SocketClientM102()];
+      
+        process.on('exit', (code:number)=>{
+          console.log('exit code',code);
+          
+          clients.forEach(v=>{
+            v.close();
+          })
+        });
+}else if(env=='vmc'){
+    const clients =[
+
+        new SocketClientVMC()];
+      
+        process.on('exit', (code:number)=>{
+          console.log('exit code',code);
+          
+          clients.forEach(v=>{
+            v.close();
+          })
+        });
+}
+
+
+
+
+    
+        
+
+
+
