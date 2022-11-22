e5cf780c94e824d763588e2c37a224ed444878e3
touy <touya.ra@gmail.com>
2022-11-22 19:16:50 +0700

    debug

--- server/api/inventoryZDM8.ts
+++ server/api/inventoryZDM8.ts
@@ -332,7 +332,7 @@ export class InventoryZDM8 implements IBaseClass {
                     axios.post<IMMoneyGenerateQRRes>('https://qr.mmoney.la/test/generateQR',
                         qr,
                         { headers: { 'mmoney-token': this.mMoneyLoginRes.token } }).then(rx => {
-                            // console.log(r);
+                             console.log('generateBillMMoney',r);
                             if (rx.status) {
                                 resolve(rx.data as IMMoneyGenerateQRRes);
                             } else {
