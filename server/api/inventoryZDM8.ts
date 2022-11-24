4a408f4195ba41b276f7a4ed3e40f827da802155
touy <touya.ra@gmail.com>
2022-11-24 14:11:00 +0700

    update

--- server/api/inventoryZDM8.ts
+++ server/api/inventoryZDM8.ts
@@ -329,7 +329,7 @@ export class InventoryZDM8 implements IBaseClass {
                 if (r) {
                     const qr = {
                         amount: value,
-                        phonenumber:this.production? this.phonenumber:'2055220199',// '2055220199',
+                        phonenumber:this.production? this.phonenumber:'2052899515',// '2055220199',
                         transactionID
                     } as IMMoneyGenerateQR;
 
