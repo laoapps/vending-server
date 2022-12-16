6d9cf4d99c5fef6ffa575a1552e7ec5ca61ce32f
touy <touya.ra@gmail.com>
2022-12-16 21:14:42 +0700

    update

--- server/api/inventoryZDM8.ts
+++ server/api/inventoryZDM8.ts
@@ -27,7 +27,7 @@ export class InventoryZDM8 implements IBaseClass {
     public phonenumber = this.production ? '2052899515' : '2054445447'; //LTC. 2058623333 
     public walletId = this.production ? '2599087166' : '2843759248';// LTC
     mmoneyusername = 'dbk';
-    mmoneypassword = 'ddbk@2022';
+    mmoneypassword = 'dbk@2022';
 
 
     constructor(router: Router, wss: WebSocketServer.Server) {
