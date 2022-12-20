5e4302232a9112b8ffd6028b0e060bd411720150
touy <touya.ra@gmail.com>
2022-12-20 16:19:43 +0700

    update production

--- server/api/inventoryZDM8.ts
+++ server/api/inventoryZDM8.ts
@@ -23,7 +23,7 @@ export class InventoryZDM8 implements IBaseClass {
 
     delayTime = 3000;
     path = '/zdm8';
-    production = false;
+    production = true;
     public phonenumber = this.production ? '2052899515' : '2054445447'; //LTC. 2058623333 
     public walletId = this.production ? '2599087166' : '2843759248';// LTC
     mmoneyusername = 'dbk';
