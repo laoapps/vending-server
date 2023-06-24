21ea9bc48a6f41dc0ca18db2d8e7e94bc7b2c5f3
smile-melody <smilemelodydev@gmail.com>
2023-06-24 20:30:01 +0700

    update

--- AdminUI/AdminUI/src/environments/environment.prod.ts
+++ AdminUI/AdminUI/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  baseurl:'http://laoapps.com:9006',
-  url:'http://laoapps.com:9006/zdm8',
-  wsurl:'ws://laoapps.com:9006/zdm8',
-  laaburl: 'http://localhost:30000/api/v1/laoapps_ewallet/',
-  testVending: 'http://localhost:30777/vending/api/',
+  baseurl:'https://vendingservicevmc.laoapps.com',
+  url:'https://vendingservicevmc.com/zdm8',
+  wsurl:'wss://vendingservicevmc.com/zdm8',
+  // laaburl: 'http://localhost:30000/api/v1/laoapps_ewallet/',
+  // testVending: 'http://localhost:30777/vending/api/',
 };
