0289c2894d55263ceffb7ea2adda5390c3619dcd
touy <touya.ra@gmail.com>
2023-06-26 02:00:32 +0700

    update

--- AdminUI/AdminUI/src/environments/environment.prod.ts
+++ AdminUI/AdminUI/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   baseurl:'https://vendingservicevmc.laoapps.com',
   url:'https://vendingservicevmc.laoapps.com/zdm8',
-  wsurl:'wss://vendingservicevmc.com/zdm8',
+  wsurl:'wss://vendingservicevmc.laoapps.com/zdm8',
   // laaburl: 'http://localhost:30000/api/v1/laoapps_ewallet/',
   // testVending: 'http://localhost:30777/vending/api/',
 };
