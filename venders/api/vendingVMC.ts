db4d4e26d2b0e6469415711ff22e68646c09dcc1
touy <touya.ra@gmail.com>
2023-06-27 16:09:28 +0700

    update

--- venders/api/vendingVMC.ts
+++ venders/api/vendingVMC.ts
@@ -63,7 +63,8 @@ export class VendingVMC {
                 console.log('INIT accept banknote');
                 that.commandVMC(EVMC_COMMAND._28, {}, -28, that.getNextNo());
                 console.log('INIT temperature');
-                // that.commandVMC(EVMC_COMMAND._7037, {}, -7037, that.getNextNo());
+                that.commandVMC(EVMC_COMMAND._7037, {}, -7037, that.getNextNo());
+                console.log('INIT temperature 2');
                 that.commandVMC(EVMC_COMMAND._7028, {}, -7028, that.getNextNo());
                 // setTimeout(() => {
                 //     console.log('INIT disable');
