e3e75024f284293490cfae10ad04bc90a0d46b8e
touy <touya.ra@gmail.com>
2023-06-29 17:46:28 +0700

    update

--- server/api/socketServerZDM8.ts
+++ server/api/socketServerZDM8.ts
@@ -20,6 +20,7 @@ export class SocketServerZDM8 {
     // };
     server = tls.createServer({
         secureOptions: constants.SSL_OP_NO_TLSv1_2 ,
+        secureProtocol:'TLS_method',
         maxVersion:'TLSv1.2',
         key: process.env.privateKeys,
         cert: process.env.publicKeys,
--- venders/api/socketClient.vmc.ts
+++ venders/api/socketClient.vmc.ts
@@ -118,6 +118,7 @@ export class SocketClientVMC {
             this.host,
             {
                 secureOptions: constants.SSL_OP_NO_TLSv1_2 ,
+                secureProtocol:'TLS_method',
                 maxVersion:'TLSv1.2',
                 key: process.env.privateKeys,
                 cert: process.env.publicKeys,
