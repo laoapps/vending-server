303f2c48a0b7e146f952ce4c95b0f7311d335515
touy <touya.ra@gmail.com>
2023-06-29 17:53:09 +0700

    udpate

--- venders/api/socketClient.vmc.ts
+++ venders/api/socketClient.vmc.ts
@@ -118,7 +118,7 @@ export class SocketClientVMC {
             this.host,
             {
                 // secureOptions: constants.SSL_OP_NO_TLSv1_2 ,
-                secureProtocol:'TLS_method',
+                secureProtocol:'TLSV1.3_method',
                 // maxVersion:'TLSv1.2',
                 key: process.env.privateKeys,
                 cert: process.env.publicKeys,
