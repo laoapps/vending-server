c4514b904b5032b3e66dd9225ea7c7e7b5956883
touy <touya.ra@gmail.com>
2023-07-02 15:29:35 +0700

    udapte url, wsurl, vending vending_server for production

--- CUI/src/environments/environment.prod.ts
+++ CUI/src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   // url : 'http://laoapps.com:9009/zdm8',
   // wsurl : 'ws://laoapps.com:9009/zdm8',
-  url : 'http://laoapps.com:9006/zdm8',
-  wsurl : 'ws://laoapps.com:9006/zdm8',
+  url : 'https://vendingservicevmc.laoapps.com/zdm8',
+  wsurl : 'wss://vendingservicevmc.laoapps.com/zdm8',
   testVending: 'http://localhost:30777/vending/api/',
-  vending: 'http://localhost:9006',
-  vending_server: 'http://laoapps.com:9006',
+  vending: 'https://vendingservicevmc.laoapps.com',
+  vending_server: 'https://vendingservicevmc.laoapps.com',
 };
