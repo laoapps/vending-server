43a280eae7b53a9087f10df6ca417719d637ffc3
touy <touya.ra@gmail.com>
2023-07-03 22:46:57 +0700

    update

--- server/services/service.ts
+++ server/services/service.ts
@@ -305,12 +305,12 @@ export function readMachineBalance(machineId: string,) {
 }
 export function readMachineSale(machineId: string) {
     // return redisClient.get('_machineSale_' + machineId);
-    return fs.readFileSync(process.cwd()+'/'+machineId,{encoding:'utf-8'});
+    return fs.readFileSync(__dirname+'/../'+machineId,{encoding:'utf-8'});
 
 }
 export function writeMachineSale(machineId: string,value:string) {
     // return redisClient.set('_machineSale_' + machineId,value);
-    const p = process.cwd()+'/'+machineId;
+    const p = __dirname+'/../'+machineId;
     fs.writeFileSync(p,value,{encoding:'utf-8'});
     return p;
 }
