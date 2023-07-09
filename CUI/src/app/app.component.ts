4ed107c7386041344d0dcbbfa48d15e0248b1fa8
touy <touya.ra@gmail.com>
2023-07-09 17:59:21 +0700

    fixed enter setting menu

--- CUI/src/app/app.component.ts
+++ CUI/src/app/app.component.ts
@@ -59,11 +59,11 @@ export class AppComponent {
       const x = prompt('password');
       console.log(x, this.getPassword());
 
-      if (!this.getPassword().endsWith(x.substring(6))||!x.startsWith(this.apiService.machineId?.otp) || x.length >=12) {
+      if (!this.getPassword().endsWith(x.substring(6))||!x.startsWith(this.apiService.machineId?.otp) || x.length < 12) return;
         this.apiService.showModal(SettingPage).then(r => {
           r.present();
         })
-      }
+      
       if (this.t) {
         clearTimeout(this.t);
         this.t = null;
