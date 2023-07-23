b67afe4f600d81defbed17c2d90a0f6b6e377769
touy <touya.ra@gmail.com>
2023-07-23 16:13:20 +0700

    test

--- venders/test.zdm8.ts
+++ venders/test.zdm8.ts
@@ -1,59 +1,60 @@
 import { SerialPort } from 'serialport';
 
-    import crc from 'crc';
+import crc from 'crc';
 
-    function checkSum(buff: any) {
-        try {
-            let x = crc.crc16modbus(Buffer.from(buff.join(''), 'hex')).toString(16);
-            x.length < 4 ? x = '0' + x : '';
-            console.log(x);
-            console.log(x.substring(2) + x.substring(0, 2));
+function checkSum(buff: any) {
+    try {
+        let x = crc.crc16modbus(Buffer.from(buff.join(''), 'hex')).toString(16);
+        x.length < 4 ? x = '0' + x : '';
+        console.log(x);
+        console.log(x.substring(2) + x.substring(0, 2));
 
-            return x.substring(2) + x.substring(0, 2);
-        }
-        catch (e) {
-            console.log('error', e);
-            return '';
-        }
+        return x.substring(2) + x.substring(0, 2);
     }
-    function int2hex(i: number) {
-        const str = Number(i).toString(16);
-        return str.length === 1 ? '0' + str : str;
+    catch (e) {
+        console.log('error', e);
+        return '';
     }
+}
+function int2hex(i: number) {
+    const str = Number(i).toString(16);
+    return str.length === 1 ? '0' + str : str;
+}
 
-    // const buff = ['01', '03', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00']
+// const buff = ['01', '03', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00']
 
-    // const x = checkSum(buff);
-    // console.log(x);
-    // /dev/tty      /dev/ttyS0  /dev/ttyS2  /dev/ttyS4    /dev/ttyUSB1  /dev/ttyUSB3
-    // /dev/ttyFIQ0  /dev/ttyS1==> ok  /dev/ttyS3  /dev/ttyUSB0  /dev/ttyUSB2  /dev/ttyUSB4
-    const path = '/dev/ttyS1';
-    const port = new SerialPort({ path: path, baudRate: 9600 }, function (err) {
-        if (err) {
-            return console.log('Error: ', err.message)
+// const x = checkSum(buff);
+// console.log(x);
+// /dev/tty      /dev/ttyS0  /dev/ttyS2  /dev/ttyS4    /dev/ttyUSB1  /dev/ttyUSB3
+// /dev/ttyFIQ0  /dev/ttyS1==> ok  /dev/ttyS3  /dev/ttyUSB0  /dev/ttyUSB2  /dev/ttyUSB4
+const path = '/dev/ttyS1';
+const port = new SerialPort({ path: path, baudRate: 9600 }, function (err) {
+    if (err) {
+        return console.log('Error: ', err.message)
+    }
+    console.log(`port ${path} accessed`);
+    var b = '';
+    port.on('data', function (data: any) {
+        console.log('data', data.toString('hex'));
+        b += new String(data);
+        console.log('buffer', b);
+    });
+
+    // 0110200100020410010100ff32
+    // const buff = ['01', '10', '20', '01', '00', '02', '04', '14', '01', '00', '00'];
+    const buff = ['01', '10', '20', '01', '00', '02', '04', '10', '01', '01', '00']
+    const check = checkSum(buff)
+    const x = buff.join('') + check;
+    console.log('x', x);
+
+    port.write(Buffer.from(x, 'hex'), (e) => {
+        if (e) {
+            console.log('command with Error: ', e.message)
+        } else {
+            console.log('command succeeded');
         }
-        console.log(`port ${path} accessed`);
-        var b = '';
-        port.on('data', function (data: any) {
-            console.log('data', data.toString('hex'));
-            b += new String(data);
-            console.log('buffer', b);
-        });
-      
-        // 0110200100020410010100ff32
-        const buff =['01','10','20','01','00','02','04','14','01','00','00'];
-        const check = checkSum(buff)
-        const x =  buff.join('') + check ;
-        console.log('x', x);
-
-        port.write(Buffer.from(x, 'hex'), (e) => {
-            if (e) {
-                console.log('command with Error: ', e.message)
-            } else {
-                console.log('command succeeded');
-            }
-        })
-     })
+    })
+})
 
 
 
