2757724acf98a5ab54ec9357089265fe70f10e7d
touy <touya.ra@gmail.com>
2023-07-28 17:20:20 +0700

    Update environment.prod.ts

--- AdminUI/AdminUI/src/environments/environment.prod.ts
+++ AdminUI/AdminUI/src/environments/environment.prod.ts
@@ -3,7 +3,8 @@ export const environment = {
   baseurl:'https://vendingservicevmc.laoapps.com',
   url:'https://vendingservicevmc.laoapps.com/zdm8',
   wsurl:'wss://vendingservicevmc.laoapps.com/zdm8',
-  filemanagerurl: 'http://localhost:27701/api/v1/file/'
+  filemanagerurl: 'https://filemanager-api.laoapps.com/api/v1/file/',
+  //filemanagerurl: 'https://localhost:27701/api/v1/file/'
   // laaburl: 'http://localhost:30000/api/v1/laoapps_ewallet/',
   // testVending: 'http://localhost:30777/vending/api/',
 };
