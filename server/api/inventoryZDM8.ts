9e761525db09583d29a03f433319c383b152267d
smile-melody <smilemelodydev@gmail.com>
2023-08-12 15:47:45 +0700

    update

--- server/api/inventoryZDM8.ts
+++ server/api/inventoryZDM8.ts
@@ -3526,7 +3526,7 @@ export class InventoryZDM8 implements IBaseClass {
 
                         axios
                             .post<IMMoneyGenerateQRRes>(
-                                "https://qr.mmoney.la/pro/GenerateQR_V2",
+                                "https://qr.mmoney.la/pro/GenerateQR_v2",
                                 qr,
                                 { headers: { 'lmm-key':'va157f35a50374ba3a07a5cfa1e7fd5d90e612fb50e3bca31661bf568dcaa5c17' } }
                             )
