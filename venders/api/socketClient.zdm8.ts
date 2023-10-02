9bef3f779339e693d57fc34c653c32102bbcaa91
touy <touya.ra@gmail.com>
2023-10-02 20:17:42 +0700

    update

--- venders/api/socketClient.zdm8.ts
+++ venders/api/socketClient.zdm8.ts
@@ -17,7 +17,7 @@ export class SocketClientZDM8 {
     // creating a custom socket client and connecting it....
     client :  tls.TLSSocket | undefined = undefined;
     // port = 51223;// new server
-    port = 51222
+    port = 51223
     host = 'laoapps.com';
     machineid = process.env.machineId || '11111111';
     otp = '111111';
