877d5f78cc65e30fabeff3b4c631cf64b1dfc12f
Souliyakv <souliyaoppo21@gmail.com>
2025-03-28 15:24:49 +0700

    Update

--- server/api/inventoryZDM8.ts
+++ server/api/inventoryZDM8.ts
@@ -611,7 +611,7 @@ export class InventoryZDM8 implements IBaseClass {
                             );
                             console.log('QR IS :', qr);
 
-                            if (qr.status != 'SUCCESS') throw new Error(EMessage.generateQRFailed);
+                            if (qr.status != 'OK') throw new Error(EMessage.generateQRFailed);
 
                             const qrData = qr.data.emv;
 
