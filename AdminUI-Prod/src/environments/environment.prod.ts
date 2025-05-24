89f0541ec6e61caa40d62892959bfb322a53985a
Souliyakv <souliyaoppo21@gmail.com>
2025-05-24 14:40:02 +0700

    Update

--- AdminUI-Prod/src/environments/environment.prod.ts
+++ AdminUI-Prod/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: true,
-    // baseurl: 'https://vendingserviceapi.laoapps.com',
-    // url: 'https://vendingserviceapi.laoapps.com/zdm8',
-    // wsurl: 'wss://vendingserviceapi.laoapps.com/zdm8',
-    // filemanagerurl: 'https://filemanager-api.laoapps.com/api/v1/file/'
-
-    baseurl: 'https://tvending.khamvong.com',
-    url: 'https://tvending.khamvong.com/zdm8',
-    wsurl: 'wss://tvending.khamvong.com/zdm8',
+    baseurl: 'https://vending-service-api5.laoapps.com',
+    url: 'https://vending-service-api5.laoapps.com/zdm8',
+    wsurl: 'wss://vending-service-api5.laoapps.com/zdm8',
     filemanagerurl: 'https://filemanager-api.laoapps.com/api/v1/file/'
+
+    // baseurl: 'https://tvending.khamvong.com',
+    // url: 'https://tvending.khamvong.com/zdm8',
+    // wsurl: 'wss://tvending.khamvong.com/zdm8',
+    // filemanagerurl: 'https://filemanager-api.laoapps.com/api/v1/file/'
 };
