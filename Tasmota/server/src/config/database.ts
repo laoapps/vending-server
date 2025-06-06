f994581b58627ba73ad09fa3eccd5765c0269781
touy <touya.ra@gmail.com>
2025-06-07 00:34:46 +0700

    update

--- Tasmota/server/src/config/database.ts
+++ Tasmota/server/src/config/database.ts
@@ -4,6 +4,9 @@ import { env } from './env';
 const sequelize = new Sequelize(env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  query:{
+    raw:false
+  }
 });
 
 export default sequelize;
