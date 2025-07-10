7fcd3533fe09e391c4b4e19d37555c81c564efee
Souliyakv <souliyaoppo21@gmail.com>
2025-07-10 14:07:06 +0700

    Update ENV

--- CUI-ProdOTA/src/environments/environment.prod.ts
+++ CUI-ProdOTA/src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  // url: 'https://vending-service-api5.laoapps.com/zdm8',
-  // wsurl: 'wss://vending-service-api5.laoapps.com/zdm8',
-  // vending_server: 'https://vending-service-api5.laoapps.com',
-  // filemanagerurl: 'https://filemanager-api.laoapps.com/api/v1/file/',
-
-  url: 'https://vendingserviceapi.laoapps.com/zdm8',
-  wsurl: 'wss://vendingserviceapi.laoapps.com/zdm8',
-  vending_server: 'https://vendingserviceapi.laoapps.com',
+  url: 'https://vending-service-api5.laoapps.com/zdm8',
+  wsurl: 'wss://vending-service-api5.laoapps.com/zdm8',
+  vending_server: 'https://vending-service-api5.laoapps.com',
   filemanagerurl: 'https://filemanager-api.laoapps.com/api/v1/file/',
+
+  // url: 'https://vendingserviceapi.laoapps.com/zdm8',
+  // wsurl: 'wss://vendingserviceapi.laoapps.com/zdm8',
+  // vending_server: 'https://vendingserviceapi.laoapps.com',
+  // filemanagerurl: 'https://filemanager-api.laoapps.com/api/v1/file/',
 
 
   // url: 'https://tvending4.khamvong.com/zdm8',
