0ebd7347e45985abe645b4923e608d9246ae43e7
touy <touya.ra@gmail.com>
2025-07-23 12:19:37 +0700

    update

--- server/api/inventoryZDM8.ts
+++ server/api/inventoryZDM8.ts
@@ -3549,29 +3549,31 @@ export class InventoryZDM8 implements IBaseClass {
         try {
             // console.log('checkSupAdmin');
             const token = req.body.token;
+            const secret = req.body.secret; 
+            
             let phoneNumber = req.body.shopPhonenumber;
             if (!token) throw new Error(EMessage.tokenNotFound);
-            if (redisClient.get)
-                findRealDB(token).then((r) => {
-                    const uuid = r;
-                    if (!uuid) throw new Error(EMessage.notfound);
-                    // req['gamerUuid'] = gamerUuid;
-                    res.locals["superadmin"] = uuid;
-                    if (phoneNumber) {
-                        phoneNumber = `+85620${phoneNumber}`;
-                        findUuidByPhoneNumberOnUserManager(phoneNumber).then(r_owneruuid => {
-                            res.locals["ownerUuid"] = r_owneruuid.uuid;
-                            next();
-                        });
-                    } else {
-                        res.locals["ownerUuid"] = uuid;
+            findRealDB(token).then((r) => {
+                const uuid = r;
+                if (!uuid) throw new Error(EMessage.notfound);
+                // req['gamerUuid'] = gamerUuid;
+                res.locals["superadmin"] = uuid;
+                
+                if (phoneNumber&&secret=='e2f48898-3453-4214-9025-27e905b269d9') {
+                    phoneNumber = `+85620${phoneNumber}`;
+                    findUuidByPhoneNumberOnUserManager(phoneNumber).then(r_owneruuid => {
+                        res.locals["ownerUuid"] = r_owneruuid.uuid;
                         next();
-                    }
-                })
-                    .catch((e) => {
-                        console.log(e);
-                        res.status(400).end();
                     });
+                } else {
+                    res.locals["ownerUuid"] = uuid;
+                    next();
+                }
+            })
+                .catch((e) => {
+                    console.log(e);
+                    res.status(400).end();
+                });
 
         } catch (error) {
             console.log(error);
