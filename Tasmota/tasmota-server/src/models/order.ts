1ef87fc7bd57cc9d867e6c79d8a1552c43c4b4be
touy <touya.ra@gmail.com>
2025-08-14 19:09:02 +0700

    update

--- Tasmota/tasmota-server/src/models/order.ts
+++ Tasmota/tasmota-server/src/models/order.ts
@@ -1,31 +1,26 @@
-import { Sequelize, DataTypes, Model, BelongsToGetAssociationMixin } from 'sequelize';
-import { SchedulePackage } from './schedulePackage';
-import { Device } from './device';
+import { Sequelize, DataTypes, Model } from 'sequelize';
 
 export interface OrderAttributes {
   id: number;
   uuid: string;
   deviceId: number;
   packageId: number;
+
   paidTime: Date;
   startedTime: Date;
   completedTime: Date;
+
   createdAt: Date;
   updatedAt: Date;
-  userUuid: string;
-  data: any;
-  relay: number;
-  conditionValue: number;
+
+
+  userUuid:string;
+  data:any;
+  relay:number
 }
 
-export interface OrderAssociations {
-  package: SchedulePackage;
-  device: Device;
-}
-
-export class Order extends Model<OrderAttributes, OrderAssociations> {
-  declare getPackage: BelongsToGetAssociationMixin<SchedulePackage>;
-  declare getDevice: BelongsToGetAssociationMixin<Device>;
+export class Order extends Model<OrderAttributes> {
+  // No public class fields, which is correct
 }
 
 export function initOrderModel(sequelize: Sequelize) {
@@ -49,24 +44,20 @@ export function initOrderModel(sequelize: Sequelize) {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
-      conditionValue: {
-        type: DataTypes.FLOAT,
-        allowNull: true,
-        defaultValue: 0,
-      },
       paidTime: { type: DataTypes.DATE, allowNull: true },
       startedTime: { type: DataTypes.DATE, allowNull: true },
       completedTime: { type: DataTypes.DATE, allowNull: true },
       createdAt: DataTypes.DATE,
       updatedAt: DataTypes.DATE,
-      userUuid: DataTypes.STRING,
-      data: DataTypes.JSONB,
-      relay: DataTypes.INTEGER,
+
+      userUuid:DataTypes.STRING,
+      data:DataTypes.JSONB,
+      relay:DataTypes.INTEGER
     },
     {
       sequelize,
       modelName: 'Order',
-      tableName: 'orders',
+      tableName: 'Orders',
     }
   );
   return Order;
