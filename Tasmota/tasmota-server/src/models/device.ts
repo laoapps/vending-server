a54039afd8ac3c50483f3dd51c6d4434c1bb5bd2
binrydara <binrydara@gmail.com>
2025-08-15 13:52:10 +0700

    up

--- Tasmota/tasmota-server/src/models/device.ts
+++ Tasmota/tasmota-server/src/models/device.ts
@@ -80,7 +80,7 @@ export function initDeviceModel(sequelize: Sequelize) {
     {
       sequelize,
       modelName: 'Device',
-      tableName: 'Devices',
+      tableName: 'devices',
     }
   );
   return Device;
