f1b758b649e098b1e734bdd8062cb7c4103c4405
Souliyakv <souliyaoppo21@gmail.com>
2025-08-23 13:27:56 +0700

    Update

--- CUI-ProdOTA/src/environments/environment.ts
+++ CUI-ProdOTA/src/environments/environment.ts
@@ -22,7 +22,7 @@ export const environment = {
   filemanagerurl: 'https://filemanager-api.laoapps.com/api/v1/file/',
   apiUrl: 'https://smartcb-api.laoapps.com/api',
 
-  versionId: '2.1.1'
+  versionId: '1.1.7'
 };
 
 /*
