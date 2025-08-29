e18d651a56c4fd322906715db275cf1f1b5c7225
binrydara <binrydara@gmail.com>
2025-08-29 17:25:01 +0700

    up

--- Tasmota/tasmota-server/src/middleware/authMiddleware.ts
+++ Tasmota/tasmota-server/src/middleware/authMiddleware.ts
@@ -31,7 +31,7 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
       }
 
       const owner = (await models.Owner.findOne({ where: { uuid: validatedUuid } }));
-      console.log('owner', owner);
+      console.log('owner444444444', owner,isOwnerFunction);
 
       let role = owner ? 'owner' : 'user';
       if (!isOwnerFunction) {
@@ -81,7 +81,7 @@ export const authHMVending = async (req: Request, res: Response, next: NextFunct
   // }
   // const ownerUuid = await validateHMVending(machineId + '', otp + '');
   const ownerUuid = await validateHMVending(token + '');
-  
+
   if (!ownerUuid) {
     res.status(401).json({ error: 'Invalid onwerUuid' });
     return;
