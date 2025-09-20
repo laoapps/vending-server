4cabe7ecccd1680de3b8cabcfd1fad6901f4ee0c
Souliyakv <souliyaoppo21@gmail.com>
2025-09-20 13:56:28 +0700

    Update

--- CUI-ProdOTA/src/app/app.component.ts
+++ CUI-ProdOTA/src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent {
   checkOnlineStatus: IAlive;
   uT = new Date();
   now = new Date();
-  version = '18';
+  version = '19';
   count = 6;
   machineuuid = this.apiService.machineuuid;
   t: any;
