0bc8e07d6a44a441e05cf149f0454ed03a2fd750
Souliyakv <souliyaoppo21@gmail.com>
2025-10-04 17:31:47 +0700

    Update

--- server-Prod/controllers/excel.controller.ts
+++ server-Prod/controllers/excel.controller.ts
@@ -3,9 +3,12 @@ import * as XLSX from "xlsx";
 import momenttz from "moment-timezone";
 import { SERVER_TIME_ZONE } from "../api/inventoryZDM8";
 import { VendingMachineBillFactory } from "../entities/vendingmachinebill.entity";
-import { EEntity, EPaymentStatus } from "../entities/system.model";
+import { EEntity, EMessage, EPaymentStatus } from "../entities/system.model";
 import { dbConnection } from "../entities";
 import { Op, where } from "sequelize";
+import https from 'https';
+import axios from "axios";
+
 
 
 export const uploadExcelFile = async (req: Request, res: Response) => {
@@ -130,7 +133,7 @@ export const uploadExcelFileAndCheckBillNotPaid = async (req: Request, res: Resp
         const worksheet = workbook.Sheets[sheetName];
 
         const ownerUuid = res.locals["ownerUuid"];
-        console.log('ownerUuid :', ownerUuid);
+        // console.log('ownerUuid :', ownerUuid);
 
         const machineId = data.machineId;
         const fromDate = momenttz.tz(data.fromDate, SERVER_TIME_ZONE).startOf('day').toDate();
@@ -140,6 +143,13 @@ export const uploadExcelFileAndCheckBillNotPaid = async (req: Request, res: Resp
         const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
         const runData = await getReportAllBill(machineId, fromDate.toString(), toDate.toString(), ownerUuid);
+        if (!runData) {
+            return res.status(400).json({
+                success: false,
+                sheetName,
+                data: [],
+            });
+        }
         const run = JSON.parse(JSON.stringify(runData ?? []));
 
 
@@ -160,38 +170,31 @@ export const uploadExcelFileAndCheckBillNotPaid = async (req: Request, res: Resp
 
         // console.log("✅ bankInMy:", bankInMy);
         // console.log("❌ bankNotInMy:", bankNotInMy);
-        // console.log("✅ myInBank:", myInBank);
+        // console.log("✅ myInBank:", myInBank); 
         // console.log("❌ myNotInBank:", myNotInBank);
-        for (let index = 0; index < myNotInBank.length; index++) {
-            const element = myNotInBank[index];
-            const transaction = element['transactionID'] ?? '';
-            console.log('transaction', transaction);
 
-            // const ent = VendingMachineBillFactory(EEntity.vendingmachinebill + '_' + ownerUuid, dbConnection);
-            // const res = await ent.findOne({
-            //     where: {
-            //         transactionID: transaction
-            //     }
-            // });
-            // console.log('res :', res);
+        const NotgetMoney: string[] = [];
 
+        await Promise.all(
+            myNotInBank.map(async (element) => {
+                const transaction = element.transactionID ?? '';
+                const resCheck = await checkQRPaidMmoneyResponse(transaction);
+                if (resCheck.status === 1) {
+                    // console.log('transaction', transaction, '✅ Success');
+                    NotgetMoney.push(transaction);
+                } else {
+                    console.log('transaction', transaction, '❌ Not Success');
 
-
-        }
-
-
-
+                }
+            })
+        );
 
         return res.status(200).json({
             success: true,
             sheetName,
-            data: jsonData,
-            run: run,
-            bankInMy: bankInMy,
-            bankNotInMy: bankNotInMy,
-            myInBank: myInBank,
-            myNotInBank: myNotInBank
+            data: NotgetMoney,
         });
+
     } catch (error: any) {
         console.error("Error reading Excel:", error);
         return res.status(500).json({
@@ -235,30 +238,80 @@ async function getReportSale(machineId: string, fromDate: string, toDate: string
 
 
 async function getReportAllBill(machineId: string, fromDate: string, toDate: string, ownerUuid: string) {
-    let condition: any = {};
-    if (machineId == 'all') {
+    try {
+        let condition: any = {};
+        if (machineId == 'all') {
 
-        condition = {
-            where: {
-                // paymentstatus: 'paid',              
-                paymentstatus: { [Op.in]: [EPaymentStatus.paid, EPaymentStatus.delivered] },
+            condition = {
+                where: {
+                    // paymentstatus: 'paid',              
+                    paymentstatus: { [Op.in]: [EPaymentStatus.paid, EPaymentStatus.delivered] },
 
-                createdAt: { [Op.between]: [fromDate, toDate] }
-            },
-            order: [['id', 'DESC']]
+                    createdAt: { [Op.between]: [fromDate, toDate] }
+                },
+                order: [['id', 'DESC']]
+            }
         }
-    }
-    else {
-        condition = {
-            where: {
-                // paymentstatus: { [Op.in]: [EPaymentStatus.paid, EPaymentStatus.delivered,] },
-                machineId: machineId,
-                createdAt: { [Op.between]: [fromDate, toDate] }
-            },
-            order: [['id', 'DESC']]
+        else {
+            condition = {
+                where: {
+                    // paymentstatus: { [Op.in]: [EPaymentStatus.paid, EPaymentStatus.delivered,] },
+                    machineId: machineId,
+                    createdAt: { [Op.between]: [fromDate, toDate] }
+                },
+                order: [['id', 'DESC']]
+            }
         }
+        const ent = VendingMachineBillFactory(EEntity.vendingmachinebill + '_' + ownerUuid, dbConnection);
+        const res = await ent.findAll(condition);
+        return res;
+    } catch (error) {
+        console.log('Error getReportAllBill :', error);
+
+        return null;
     }
-    const ent = VendingMachineBillFactory(EEntity.vendingmachinebill + '_' + ownerUuid, dbConnection);
-    const res = await ent.findAll(condition);
-    return res;
+}
+
+
+
+function checkQRPaidMmoneyResponse(transactionID: string): Promise<{ status: number, message: any }> {
+    return new Promise<{ status: number, message: any }>(async (resolve, reject) => {
+
+        try {
+            const agent = new https.Agent({
+                rejectUnauthorized: false,
+            });
+
+            const API_BASE_URL = 'https://gateway.ltcdev.la/PartnerGenerateQR/checkTransactionByBillV3';
+
+            const authUsername = 'lmm'
+            const authPassword = 'Lmm@2024qaz2wsx'
+
+            const authToken = Buffer.from(`${authUsername}:${authPassword}`).toString('base64');
+
+            const headers = {
+                "Authorization": `Basic ${authToken}`,
+                "username": 'Vendeex',
+                "password": 'vendeex@2025qaz2wsx',
+                "apikey": 'eb718666-b20e-4091-b964-67a61e06fffe',
+                "Content-Type": 'application/json'
+            }
+
+            const res = await axios.post(API_BASE_URL, {
+                requestId: transactionID,
+                billNumber: transactionID
+            },
+                { headers, httpsAgent: agent, timeout: 10000 });
+
+            if (res.data.success) {
+                resolve({ status: 1, message: res.data })
+            } else {
+                // console.log('=====> CHECK MMONEY NOT PAID', res.data);
+                return resolve({ status: 0, message: EMessage.LaoQRNotPaid });
+            }
+        } catch (error) {
+            console.log('=====> CHECK MMONEY PAID ERROR', error);
+            return resolve({ status: 0, message: error });
+        }
+    });
 }
