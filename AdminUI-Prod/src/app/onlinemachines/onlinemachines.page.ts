e10ae777bde51deea3be7a0bf02dc7d986f85bff
Souliyakv <souliyaoppo21@gmail.com>
2025-10-23 16:20:44 +0700

    Update

--- AdminUI-Prod/src/app/onlinemachines/onlinemachines.page.ts
+++ AdminUI-Prod/src/app/onlinemachines/onlinemachines.page.ts
@@ -182,6 +182,24 @@ export class OnlinemachinesPage implements OnInit, OnDestroy {
     }
   }
 
+
+  async clearClientLogs() {
+    try {
+      const token = localStorage.getItem('token');
+      await axios.post(`${environment.url}/clearClientLogs`, {
+        token,
+      }).then(r => {
+        this.apiService.alertSuccess('ລົບຂໍ້ມູນສຳເຫຼັດ');
+      }).catch(err => {
+        this.apiService.alertError(err);
+      });
+
+    } catch (error) {
+      console.error('Error clearLogTemp:', error);
+      alert(`Error clearLogTemp: ${error.message}`);
+    }
+  }
+
   async refreshMachine(machineId: string) {
     try {
       const token = localStorage.getItem('token');
