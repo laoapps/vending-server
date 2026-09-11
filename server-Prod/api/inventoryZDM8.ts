964eb6628dc20f57e9a11af7be214cf094fd32e5
touy <touya.ra@gmail.com>
2026-09-11 17:44:08 +0700

    debug

--- server-Prod/api/inventoryZDM8.ts
+++ server-Prod/api/inventoryZDM8.ts
@@ -1415,7 +1415,7 @@ export class InventoryZDM8 implements IBaseClass {
                         const sEnt = ProductShowcaseFactory(EEntity.productshowcase + '_' + ownerUuid, dbConnection);
                         await sEnt.sync();
                         const row = await sEnt.findOne({ where: { stockId: p.id } });
-                        res.send(PrintSucceeded('productShowcaseByImage 2'+`${JSON.stringify(row.toJSON())} ${EEntity.product + '_' + ownerUuid} ${image}`, row ? [row] : [], EMessage.succeeded, returnLog(req, res)));
+                        res.send(PrintSucceeded('productShowcaseByImage 2'+`${JSON.stringify(row)} ${EEntity.product + '_' + ownerUuid} ${image}`, row ? [row] : [], EMessage.succeeded, returnLog(req, res)));
                     } catch (error) {
                         res.send(PrintError('productShowcaseByImage', error, EMessage.error, returnLog(req, res, true)));
                     }
@@ -1446,7 +1446,7 @@ export class InventoryZDM8 implements IBaseClass {
                         const sEnt = ProductShowcaseFactory(EEntity.productshowcase + '_' + ownerUuid, dbConnection);
                         await sEnt.sync();
                         const row = await sEnt.findOne({ where: { stockId: p.id } });
-                        res.send(PrintSucceeded('productShowcaseListByImage '+`${JSON.stringify(row.toJSON())} ${EEntity.product + '_' + ownerUuid} ${image}`, row ? [row] : [], EMessage.succeeded, returnLog(req, res)));
+                        res.send(PrintSucceeded('productShowcaseListByImage '+`${JSON.stringify(row)} ${EEntity.product + '_' + ownerUuid} ${image}`, row ? [row] : [], EMessage.succeeded, returnLog(req, res)));
                     } catch (error) {
                         res.send(PrintError('productShowcaseListByImage', error, EMessage.error, returnLog(req, res, true)));
                     }
